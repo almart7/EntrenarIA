3724eebf4e3fead4a6297a4ce40e700dec29ee1b
almart7 <alessandra.martinez.m@gmail.com>
2025-05-20 16:47:11 +0200

    Logo.

--- app/src/main/java/com/alessandra/entrenaria/ui/screens/initial/InitialScreen.kt
+++ app/src/main/java/com/alessandra/entrenaria/ui/screens/initial/InitialScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -139,7 +140,7 @@ fun InitialScreen(
             // Eslogan
             Text(
                 "Entrena de forma inteligente",
-                color = MaterialTheme.colorScheme.onPrimary,
+                color = Color.White,
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 textAlign = TextAlign.Center,
